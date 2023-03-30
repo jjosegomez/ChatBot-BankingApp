2cc105803f850a4e5bbda70fd4395fdd5035f22c
jjosegomez <90978460+jjosegomez@users.noreply.github.com>
2023-03-30 19:13:41 -0400

    chat bug fixed
    we need to connect chat bot to the chatUI

--- frontend/src/layouts/dashboard/DashboardLayout.js
+++ frontend/src/layouts/dashboard/DashboardLayout.js
@@ -5,7 +5,7 @@ import { styled } from '@mui/material/styles';
 //
 import Header from './header';
 import Nav from './nav';
-import ChatButton from './nav/ChatButton';import Chat from './nav/Chat.js';
+import ChatButton from './nav/ChatButton';import Chat from './nav/ChatButton';
 // ----------------------------------------------------------------------
 
 const APP_BAR_MOBILE = 64;
