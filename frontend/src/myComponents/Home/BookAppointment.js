cd34cb73cd4d6a7d6b0915086ed87b59ae70026f
Juan Jose Gomez Medina <juanjo.gomez2000@hotmail.com>
2023-04-04 16:05:03 -0400

    Homepage changed

---
+++ frontend/src/myComponents/Home/BookAppointment.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Typography, Box, Container} from '@mui/material';
+
+const BookAppointment = () => {
+  return (
+    <>
+        <Typography sx={{maxWidth:"1000px"}}>
+            We offer personalized coaching services to help you achieve your goals and unlock your full potential. Our experienced coaches provide tailored support and guidance, whether you're looking to improve your career, relationships, health, or overall well-being. With a proven track record of delivering measurable results, we're confident we can help you achieve lasting success. If you're ready to take the next step, schedule a consultation with one of our expert coaches today.
+        </Typography>
+    </>
+  );
+};
+
+export default BookAppointment;
---
+++ frontend/src/myComponents/Home/Welcome.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Typography, Box, Container} from '@mui/material';
+
+const Welcome = () => {
+  return (
+    <>
+    <Box sx={{m:5, display: "flex", flexFlow: "row wrap", justifyContent:"space-around", alignItems:"center"}}>
+        <Box sx={{display:"flex", flexFlow: 'column',maxWidth:"500px"}}>
+            <Typography sx={{ m:5, mb:1, p:0}} color="textPrimary" variant="h3">
+                Find Your Perfect Life Coach Today!
+            </Typography>
+            <Typography sx={{m:5, mt:2.5}}>
+                Welcome to MindfulCoach, where you can find your perfect life coach. We help match you with a coach who understands your personality and goals, so you can achieve your full potential. Our easy-to-use platform lets you search for coaches based on your preferences and connect with them directly to schedule sessions. Let us help you find the support and guidance you need to succeed.
+            </Typography>
+        </Box>
+        <Box sx={{display:"flex", justifyContent:"center", alignItems:"center", overflow:"hidden"}}>
+            <Box sx={{maxWidth:"500px", borderRadius:"5px", overflow:'hidden'}}>
+                <img src='https://fastly.picsum.photos/id/28/4928/3264.jpg?hmac=GnYF-RnBUg44PFfU5pcw_Qs0ReOyStdnZ8MtQWJqTfA'></img>
+            </Box>
+        </Box>
+    </Box>
+    </>
+  );
+};
+
+export default Welcome;
+
